90f1c9a1e0bec4ac21e22340c19812cc0a50c71d
rpass <robertmpassmore@gmail.com>
2016-02-10 18:39:35 +0200

    Fixed: hamming passed 1st test.

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -4,6 +4,10 @@
 var hamming;
 hamming = function () {};
 
-hamming.prototype.hello = function () {
-    return '';
+hamming.prototype.compute = function (arg1, arg2) {
+    if(arg1 === arg2){
+        return 0;
+    }
 };
+
+module.exports = hamming;
