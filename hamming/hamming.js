6c5325a26e8658c2c72263ab9cfbf8b906067091
rpass <robertmpassmore@gmail.com>
2016-02-12 08:08:48 +0200

    Fixed: hamming.js passed all tests.

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -2,12 +2,27 @@
  * Created by robert.passmore on 2016/02/10.
  */
 var hamming;
-hamming = function () {};
+hamming = function () {
+};
+
 
 hamming.prototype.compute = function (arg1, arg2) {
-    if(arg1 === arg2){
-        return 0;
+    var areDifferent = function (char1, char2) {
+        return char1 !== char2;
+    };
+
+    var DifferentLengthError = new Error('DNA strands must be of equal length.');
+
+    var hamDistance = 0;
+
+    if(arg1.length !== arg2.length) {
+        throw DifferentLengthError;
+    } else {
+        for(var i=0; i<arg1.length; i++){
+            hamDistance += areDifferent(arg1.charAt(i), arg2.charAt(i));
+        }
     }
+    return hamDistance;
 };
 
 module.exports = hamming;
